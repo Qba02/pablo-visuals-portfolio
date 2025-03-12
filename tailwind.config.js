290e2afbb988d217d52d49153bbd35f60ef88ebb
Jakub Nowak <njakub002@gmail.com>
2025-03-12 14:10:39 +0100

    Tailwind config fixed (v3 instead of v4 installed)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#d50000",
+        secondary: "#850101",
+        tertiary: "#444444",
+        dark: "#100d0c",
+        light: "#ededed",
+      },
+    },
+  },
+  plugins: [],
+};
