41dfa300d9b75f92e0b31f01f2812f44a7827a0d
Jakub Nowak <njakub002@gmail.com>
2025-04-13 13:44:51 +0200

    Refactor - project png names changed and unused bg removed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,16 +1,14 @@
 import logo from "./logo.svg";
 import heroBg from "./hero-bg.png";
-import heroBg2 from "./hero-bg-2.png";
 import profilePic from "./profile-pic.png";
-import projectPlot from "./projects/fabularny.png";
-import projectMusic from "./projects/teledysk.jpg";
-import projectDocument from "./projects/dokument.png";
-import projectAd from "./projects/reklama.png";
+import projectPlot from "./projects/fictional.png";
+import projectMusic from "./projects/music-video.jpg";
+import projectDocument from "./projects/document.png";
+import projectAd from "./projects/ad.png";
 
 export {
   logo,
   heroBg,
-  heroBg2,
   profilePic,
   projectPlot,
   projectMusic,
